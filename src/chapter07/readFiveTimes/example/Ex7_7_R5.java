56a3efe76d31f1b04a0e5b6c7a180e6dcb643c35
Son-Gyeongi <thsruddl77@gmail.com>
2025-02-02 13:21:00 +0900

    chapter07 Ex

---
+++ src/chapter07/readFiveTimes/example/Ex7_7_R5.java
@@ -0,0 +1,36 @@
+package chapter07.readFiveTimes.example;
+
+public class Ex7_7_R5 {
+    public static void main(String[] args) {
+        Car_7_7 car = null;
+        FireEngine7_7 fe = new FireEngine7_7();
+        FireEngine7_7 fe2 = null;
+
+        fe.water();
+        // 사용할 수 있는 멤버의 개수 조절
+        car = fe;
+//        car.water(); // Car 타입의 참조변수로 water() 호출할 수 없다.
+        fe2 = (FireEngine7_7) car;
+        fe2.water();
+    }
+}
+
+class Car_7_7 {
+    String color;
+    int door;
+
+    void drive() { // 운전하는 기능
+        System.out.println("drive, Brrrrr~");
+    }
+
+    void stop() { // 멈추는 기능
+        System.out.println("stop!!!");
+    }
+}
+
+class FireEngine7_7 extends Car_7_7 { // 소방차
+    void water() {
+        // 물 뿌리는 기능
+        System.out.println("water!!!");
+    }
+}
