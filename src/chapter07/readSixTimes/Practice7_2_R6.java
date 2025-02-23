79c505c649e297b4b32c7c245767f3fd285d066a
Son-Gyeongi <thsruddl77@gmail.com>
2025-02-23 19:09:26 +0900

    chapter07 Practice add 6th 1

--- src/chapter07/readSixTimes/Practice7_2_R6.java
+++ src/chapter07/readSixTimes/Practice7_2_R6.java
@@ -6,6 +6,7 @@ public class Practice7_2_R6 {
 
         System.out.println(deck.pick(0));
         System.out.println(deck.pick());
+        System.out.println(deck.pick(30)); // 유효하지 않은 인덱스 값이 주어질 경우
         deck.shuffle();
 
         for (int i = 0; i < deck.cards.length; i++) {
