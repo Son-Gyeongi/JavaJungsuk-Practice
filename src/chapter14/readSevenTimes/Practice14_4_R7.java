893a1549be2825229a4508612d77b410a1874590
Son-Gyeongi <thsruddl77@gmail.com>
2025-03-19 18:32:47 +0900

    chapter14 Practice Answer 7th

--- src/chapter14/readSevenTimes/Practice14_4_R7.java
+++ src/chapter14/readSevenTimes/Practice14_4_R7.java
@@ -2,6 +2,7 @@ package chapter14.readSevenTimes;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Practice14_4_R7 {
     public static void main(String[] args) {
@@ -12,5 +13,12 @@ public class Practice14_4_R7 {
                                                 .mapToObj(i2 -> new int[]{i1, i2}))
                 .filter(arr -> arr[0] + arr[1] == 6)
                 .forEach(arr -> System.out.println(Arrays.toString(arr)));
+
+        System.out.println();
+
+        IntStream.rangeClosed(1, 6).boxed()
+                .flatMap(i1 -> Stream.of(1, 2, 3, 4, 5, 6).map(i2 -> new int[]{i1, i2}))
+                .filter(iArr -> iArr[0] + iArr[1] == 6)
+                .forEach(iArr -> System.out.println(Arrays.toString(iArr)));
     }
 }
